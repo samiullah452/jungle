08c85f894b6e36bb4fcb297e8b6ca40ec0d47f38
samiullah ilyas <m.sameeullahilyas@gmail.com>
2019-11-21 06:17:10 +0500

    f

--- JungleScout/mysite/views.py
+++ JungleScout/mysite/views.py
@@ -214,7 +214,7 @@ def profitcalculator(request):
                 os.mkdir(os.path.join(dirname, request.user.username))
         except:
             pass
-
+        dir_list= (next(os.walk(settings.MEDIA_ROOT + "\\users"))[1])
         user_dir_list = dir_list[dir_list.index(request.user.username)]
         user_dir_list= next(os.walk(settings.MEDIA_ROOT + "\\users" + "\\" + user_dir_list))[1]
 
@@ -347,6 +347,7 @@ def supplier_finder(request):
                 os.mkdir(os.path.join(dirname, request.user.username))
         except:
             pass
+        dir_list= (next(os.walk(settings.MEDIA_ROOT + "\\users"))[1])
         user_dir_list = dir_list[dir_list.index(request.user.username)]
         user_dir_list= next(os.walk(settings.MEDIA_ROOT + "\\users" + "\\" + user_dir_list))[1]
         overview={}
@@ -398,6 +399,7 @@ def home(request):
                 os.mkdir(os.path.join(dirname, request.user.username))
         except:
             pass
+        dir_list= (next(os.walk(settings.MEDIA_ROOT + "\\users"))[1])
         user_dir_list = dir_list[dir_list.index(request.user.username)]
         user_dir_list= next(os.walk(settings.MEDIA_ROOT + "\\users" + "\\" + user_dir_list))[1]
         overview={}
@@ -450,6 +452,7 @@ def keyword_view(request):
                 os.mkdir(os.path.join(dirname, request.user.username))
         except:
             pass
+        dir_list= (next(os.walk(settings.MEDIA_ROOT + "\\users"))[1])
         user_dir_list = dir_list[dir_list.index(request.user.username)]
         user_dir_list= next(os.walk(settings.MEDIA_ROOT + "\\users" + "\\" + user_dir_list))[1]
         overview={}
