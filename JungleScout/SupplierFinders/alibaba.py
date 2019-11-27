f33727ec8fe20ca37583f23b55908b40445f1c4e
samiullah ilyas <m.sameeullahilyas@gmail.com>
2019-11-27 23:22:51 +0500

    f

--- JungleScout/SupplierFinders/alibaba.py
+++ JungleScout/SupplierFinders/alibaba.py
@@ -1,4 +1,3 @@
-def alibaba(urls):
 	import requests
 	url = 'https://api.exchangerate-api.com/v4/latest/CNY'
 	response = requests.get(url)	
@@ -6,52 +5,45 @@ def alibaba(urls):
 	rate=data["rates"]["USD"]
 	ratekrw=data["rates"]["KRW"]
 
-	from selenium import webdriver
 	import time
 	import string
 	import os
-	import urllib.request
-	from selenium.webdriver.chrome.options import Options
-	options = Options()
-	options.add_argument('--headless')
-	options.add_argument('--no-sandbox')
-	options.add_argument('--disable-dev-shm-usage')
+	from bs4 import BeautifulSoup
 	content={}
 	i=0
-	driver = webdriver.Chrome('contentdownloader/chromedriver',options=options)
 	for url in urls:
 		try:
-			print(urls[url]['url'])
 			content[i]={}
 			content[i]['group']='Uncategorized'			    
 			content[i]['image']=[]
 			content[i]['region']=urls[url]['region']
-			driver.get(urls[url]['url'])
-			name = driver.find_elements_by_class_name('company-name-container')
-
+			response=requests.get(urls[url]['url'])
+			soup = BeautifulSoup(response.content, 'html5lib')
+			name = soup.findAll("div",{"class":'company-name-container'})
 			for name in name:
-				content[i]['company_href']=name.find_element_by_xpath(".//a").get_attribute('href')
+				content[i]['company_href']=(name.find("a"))['href']
 				content[i]['shop-name']=name.text
 
-			images=driver.find_elements_by_xpath('//div[@class="thumb"]/a/img')
+			images=soup.findAll("div",{"class":"thumb"})
 			j=0
 			for image in images:
-				try:
 					if j==3:
 						break
-					image.click()
-					imgs = driver.find_elements_by_xpath('//div[@class="iwrap nopic"]/a/img')
-					for img in imgs:
-						content[i]['image'].append(img.get_attribute('src'))
+					imgs = image.find("img")['src']
+					imgs.find(".jpg")
+					if imgs.find(".jpg")!=-1:
+						content[i]['image'].append(imgs[:imgs.find(".jpg")]+".jpg")
 						j+=1
-				except Exception as e:
-					pass
+					elif imgs.find(".png")!=-1:
+						content[i]['image'].append(imgs[:imgs.find(".jpg")]+".png")
+						j+=1
 
-			title = driver.find_elements_by_class_name('ma-title')
+
+			title = soup.findAll("div",{"class":'ma-title'})
 			for title in title:
 			    content[i]['title']=title.text
 
-			price = driver.find_elements_by_class_name('ma-price-wrap')
+			price = soup.findAll("div",{"class":'ma-price-wrap'})
 			for price in price:
 				index=(price.text).find("$")
 				if index==-1:
@@ -67,7 +59,7 @@ def alibaba(urls):
 				content[i]['price']=round((1/rate)*float(content_price), 2)
 
 			if 'price' not in content[i]:
-				price = driver.find_elements_by_class_name('ma-reference-price-highlight')
+				price = driver.findAll("div",{"class":'ma-reference-price-highlight'})
 				for price in price:
 					index=(price.text).find("$")
 					if index==-1:
@@ -82,7 +74,7 @@ def alibaba(urls):
 
 
 			if 'price' not in content[i]:
-				price = driver.find_elements_by_class_name('pre-inquiry-price')
+				price = soup.findAll("div",{"class":'pre-inquiry-price'})
 				for price in price:
 					index=(price.text).find("$")
 					if index==-1:
